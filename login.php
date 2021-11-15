baa00d6e89711132ed97e23a067110d11dcde5d0
aswin-syras <aswinsyras@gmail.com>
2021-11-14 23:16:40 -0500

    admin authentication

--- login.php
+++ login.php
@@ -9,21 +9,27 @@ session_start();
 		$user_name = $_POST['user_name'];
 		$password = $_POST['password'];
 
+
 		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
 		{
-			$query = " select * from users where user_name = '$user_name' limit 1";
-			$result = mysqli_query($con,$query);
+			if($user_name == "admin" && $password = "password"){
+				header("Location: index.php");
+			}
+			else{
+				$query = " select * from users where user_name = '$user_name' limit 1";
+				$result = mysqli_query($con,$query);
 
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
+				if($result)
 				{
-					$user_data = mysqli_fetch_assoc($result);
-					if($user_data['password'] === $password)
+					if($result && mysqli_num_rows($result) > 0)
 					{
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: booking.php");
-						die;
+						$user_data = mysqli_fetch_assoc($result);
+						if($user_data['password'] === $password)
+						{
+							$_SESSION['user_id'] = $user_data['user_id'];
+							header("Location: booking.php");
+							die;
+						}
 					}
 				}
 			}
